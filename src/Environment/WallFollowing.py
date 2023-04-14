03b21fc19618c51ecd52f43dccc4851ecfd5854e
José Pedro Carvalho <up201806263@fe.up.pt>
2023-04-14 11:46:11 +0100

    Improved WF

--- src/Environment/WallFollowing.py
+++ src/Environment/WallFollowing.py
@@ -1,114 +1,101 @@
 from src.Environment.Actions import *
 import numpy as np
+import random
 
 
 class WallFollower:
     def __init__(self):
-
-        self.clockwise = None
-        self.lastAction = None
         self.size = None
         self.n_actions = None
-        self.found_wall = None
-        self.found_second_wall = False
+        self.action_seq = []
+        self.action_idx = 0
+        self.center = None
+        self.north = None
+        self.south = None
+        self.west = None
+        self.east = None
+        self.wall_list = []
+        self.dirs = None
         self.counter = 0
-        self.second_wall = None
-        self.second_action = None
 
     def select_action(self, obs):
 
-        center = obs.shape[1] // 2
-        north = (center - 1, center)
-        south = (center + 1, center)
-        west = (center, center - 1)
-        east = (center, center + 1)
+        if len(self.wall_list) < 2:
+            for d in self.dirs:
+                if obs[2][d] == 1:
+                    if d not in self.wall_list:
+                        self.wall_list.append(d)
+                        self.assign_actions()
 
-        if not self.found_second_wall:
-            if not self.found_wall:
-                if obs[2][north] == 1:
-                    self.found_wall = True
-                    self.second_action = Actions.SOUTH
-                    self.lastAction = Actions.WEST
-                    self.second_wall = west
-            if self.found_wall:
-                if obs[2][self.second_wall] == 1:
-                    self.found_second_wall = True
-                    self.lastAction = self.second_action
-
-            if self.found_second_wall:
-                self.n_actions = self.size
-        else:
+        if len(self.wall_list) == 2:
+            a = self.action_seq[self.counter % len(self.action_seq)]
             self.n_actions -= 1
-            if self.n_actions == 0 or (self.n_actions == 1 and self.counter == 3):
+            if self.n_actions == 1 and self.counter == 3:
+                self.counter = 0
+                self.size -= 2
+                self.n_actions = self.size +1
+            if self.n_actions == 0:
+                self.n_actions = self.size
                 self.counter += 1
-                if self.counter == 4 and self.n_actions == 1:
-                    self.size -= 2
-                    self.counter = 0
-                self.n_actions += self.size
-                if self.lastAction == Actions.NORTH:
-                    self.lastAction = Actions.WEST
-                elif self.lastAction == Actions.WEST:
-                    self.lastAction = Actions.SOUTH
-                elif self.lastAction == Actions.SOUTH:
-                    self.lastAction = Actions.EAST
-                elif self.lastAction == Actions.EAST:
-                    self.lastAction = Actions.NORTH
-        print(self.lastAction, self.n_actions, self.size, self.counter, self.found_wall, self.found_second_wall)
-        return self.lastAction
+
+        action = self.action_seq[self.counter]
+        print(action, self.counter,self.n_actions)
+        return action
 
     def init(self, obs, size):
+        self.center = obs.shape[1] // 2
+        self.north = (self.center - 1, self.center)
+        self.south = (self.center + 1, self.center)
+        self.west = (self.center, self.center - 1)
+        self.east = (self.center, self.center + 1)
+        self.dirs = [self.north, self.south, self.west, self.east]
+        self.counter = 0
+        self.wall_list = []
         self.n_actions = size
         self.size = size - 1
-        center = obs.shape[1] // 2
-        north = (center - 1, center)
-        south = (center + 1, center)
-        west = (center, center - 1)
-        east = (center, center + 1)
-        self.found_wall = False
-        self.found_second_wall = False
-        self.counter = 0
-        self.second_wall = False
-        self.second_action = False
 
-        if obs[2][north] == 1:
-            self.found_wall = True
-            if obs[2][east] == obs[2][west] == 0:
-                self.lastAction = Actions.WEST
-                self.second_wall = west
-                self.second_action = Actions.SOUTH
-            elif obs[2][east] == 1:
-                self.lastAction = Actions.WEST
-                self.found_second_wall = True
-            elif obs[2][west] == 1:
-                self.lastAction = Actions.SOUTH
-                self.found_second_wall = True
+        if obs[2][self.north] == 1:
+            self.wall_list.append(self.north)
+        if obs[2][self.south] == 1:
+            self.wall_list.append(self.south)
+        if obs[2][self.west]:
+            self.wall_list.append(self.west)
+        if obs[2][self.east]:
+            self.wall_list.append(self.east)
 
-        elif obs[2][south] == 1:
-            self.found_wall = True
-            if obs[2][east] == obs[2][west] == 0:
-                self.lastAction = Actions.EAST
-                self.second_wall = east
-                self.second_action = Actions.WEST
+        self.assign_actions()
 
-            elif obs[2][east] == 1:
-                self.lastAction = Actions.NORTH
-                self.found_second_wall = True
-            elif obs[2][west] == 1:
-                self.lastAction = Actions.EAST
-                self.found_second_wall = True
+    def assign_actions(self):
+        if len(self.wall_list) == 2:
+            if self.north in self.wall_list and self.east in self.wall_list:
+                self.action_seq = random.choice([[Actions.SOUTH, Actions.WEST, Actions.NORTH, Actions.EAST],
+                                                 [Actions.WEST, Actions.SOUTH, Actions.EAST, Actions.NORTH]])
 
-        elif obs[2][west]:
-            self.lastAction = Actions.SOUTH
-            self.found_wall = True
-            self.second_wall = south
-            self.second_action = Actions.EAST
-        elif obs[2][east]:
-            self.lastAction = Actions.NORTH
-            self.found_wall = True
-            self.second_wall = north
-            self.second_action = Actions.WEST
+            elif self.north in self.wall_list and self.west in self.wall_list:
+                l1 = [Actions.SOUTH, Actions.EAST, Actions.NORTH, Actions.WEST]
+                l2 = [Actions.EAST, Actions.SOUTH, Actions.WEST, Actions.NORTH]
+                self.action_seq = random.choice([l1, l2])
+
+            elif self.south in self.wall_list and self.east in self.wall_list:
+                self.action_seq = random.choice([[Actions.NORTH, Actions.WEST, Actions.SOUTH, Actions.EAST],
+                                                 [Actions.WEST, Actions.NORTH, Actions.EAST, Actions.SOUTH]])
+
+            elif self.south in self.wall_list and self.west in self.wall_list:
+                self.action_seq = random.choice([[Actions.NORTH, Actions.EAST, Actions.SOUTH, Actions.WEST],
+                                                 [Actions.EAST, Actions.NORTH, Actions.WEST, Actions.SOUTH]])
+
+        elif len(self.wall_list) == 1:
+            if self.north in self.wall_list:
+                self.action_seq = [random.choice([Actions.WEST, Actions.EAST])]
+
+            elif self.west in self.wall_list:
+                self.action_seq = [random.choice([Actions.SOUTH, Actions.NORTH])]
+
+            elif self.east in self.wall_list:
+                self.action_seq = [random.choice([Actions.NORTH, Actions.SOUTH])]
+
+            elif self.south in self.wall_list:
+                self.action_seq = [random.choice([Actions.WEST, Actions.EAST])]
 
         else:
-            self.lastAction = Actions.NORTH
-            self.found_wall = False
-            self.found_second_wall = False
+            self.action_seq = [random.choice(list(Actions))]
