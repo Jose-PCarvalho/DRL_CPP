efc3a68e12be1363bf13cec1d97aa5c640d93b6b
José Pedro Carvalho <up201806263@fe.up.pt>
2023-05-04 12:24:33 -0700

    solved torch.clamp precision problem

--- src/Rainbow/agent.py
+++ src/Rainbow/agent.py
@@ -11,11 +11,11 @@ from src.Rainbow.model import DQN
 class Agent:
     def __init__(self, args, action_space):
         self.action_space = action_space
-        self.atoms = args.atoms
+        self.atoms = int(args.atoms)
         self.Vmin = args.V_min
         self.Vmax = args.V_max
-        self.support = torch.linspace(args.V_min, args.V_max, self.atoms).to(device=args.device)  # Support (range) of z
-        self.delta_z = (args.V_max - args.V_min) / (self.atoms - 1)
+        self.support = torch.linspace(self.Vmin, self.Vmax, self.atoms).to(device=args.device)  # Support (range) of z
+        self.delta_z = (self.Vmax - self.Vmin) / (self.atoms - 1)
         self.batch_size = args.batch_size
         self.n = args.multi_step
         self.discount = args.discount
@@ -77,8 +77,7 @@ class Agent:
             # Calculate nth next state probabilities
             pns = self.online_net(next_states, next_battery)  # Probabilities p(s_t+n, ·; θonline)
             dns = self.support.expand_as(pns) * pns  # Distribution d_t+n = (z, p(s_t+n, ·; θonline))
-            argmax_indices_ns = dns.sum(2).argmax(
-                1)  # Perform argmax action selection using online network: argmax_a[(z, p(s_t+n, a; θonline))]
+            argmax_indices_ns = dns.sum(2).argmax(1)  # Perform argmax action selection using online network: argmax_a[(z, p(s_t+n, a; θonline))]
             self.target_net.reset_noise()  # Sample new target net noise
             pns = self.target_net(next_states, next_battery)  # Probabilities p(s_t+n, ·; θtarget)
             pns_a = pns[range(
@@ -90,19 +89,18 @@ class Agent:
             Tz = Tz.clamp(min=self.Vmin, max=self.Vmax)  # Clamp between supported values
             # Compute L2 projection of Tz onto fixed support z
             b = (Tz - self.Vmin) / self.delta_z  # b = (Tz - Vmin) / Δz
+            b=b.clamp(max=self.atoms-1)
             l, u = b.floor().to(torch.int64), b.ceil().to(torch.int64)
             # Fix disappearing probability mass when l = b = u (b is int)
             l[(u > 0) * (l == u)] -= 1
-            u[(l < (self.atoms - 1)) * (l == u)] += 1
-
-            # Distribute probability of Tz
+            u[(l < (self.atoms - 1)) * (l == u)] += 1            # Distribute probability of Tz
             m = states.new_zeros(self.batch_size, self.atoms)
             offset = torch.linspace(0, ((self.batch_size - 1) * self.atoms), self.batch_size).unsqueeze(1).expand(
                 self.batch_size, self.atoms).to(actions)
             m.view(-1).index_add_(0, (l + offset).view(-1),
                                   (pns_a * (u.float() - b)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
-            m.view(-1).index_add_(0, (u + offset).view(-1),
-                                  (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
+            m.view(-1).index_add_(0,(u + offset).view(-1),
+                               (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
 
         loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
         self.online_net.zero_grad()
@@ -131,7 +129,7 @@ class Agent:
         self.online_net.eval()
 
     def update_C51(self, size):
-        self.Vmin = -size ** 2
-        self.Vmax = size ** 2
+        self.Vmin = -size**2-size
+        self.Vmax = 361.0+size
         self.support = torch.linspace(self.Vmin, self.Vmax, self.atoms).to(device=self.device)  # Support (range) of z
         self.delta_z = (self.Vmax - self.Vmin) / (self.atoms - 1)
